b189f0942caf8b732660eb56acbb46d04a2fe2f5
Peter Andersson <pa222ku@student.lnu.se>
2016-09-13 21:10:07 +0200

    Fix test case 1.1

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,7 +5,7 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		$timeString = date("l") . ", the " . date("j") . "th of " . date("F") . " " . date("Y") . ", The time is " . date("H") . ":" . date("i") . ":" . date("s");
 
 		return '<p>' . $timeString . '</p>';
 	}
