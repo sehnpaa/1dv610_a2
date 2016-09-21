0991de4f8489a64dfdf6669e712aa52adb67e249
Peter Andersson <pa222ku@student.lnu.se>
2016-09-21 09:26:43 +0200

    Fix suddenly failing test case 1.1

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,7 +5,7 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = date("l") . ", the " . date("j") . "th of " . date("F") . " " . date("Y") . ", The time is " . date("H") . ":" . date("i") . ":" . date("s");
+		$timeString = date("l") . ", the " . date("jS") . " of " . date("F") . " " . date("Y") . ", The time is " . date("H") . ":" . date("i") . ":" . date("s");
 
 		return '<p>' . $timeString . '</p>';
 	}
