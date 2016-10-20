c053a343b5638044a58929bc588fb4e549f89417
Peter Andersson <pa222ku@student.lnu.se>
2016-10-20 11:19:25 +0200

    Break up long line in DateTimeView.php

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -7,7 +7,8 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = date("l") . ", the " . date("jS") . " of " . date("F") . " " . date("Y") . ", The time is " . date("H") . ":" . date("i") . ":" . date("s");
+		$timeString = date("l") . ", the " . date("jS") . " of " . date("F") . " " . date("Y")
+			. ", The time is " . date("H") . ":" . date("i") . ":" . date("s");
 
 		return '<p>' . $timeString . '</p>';
 	}
